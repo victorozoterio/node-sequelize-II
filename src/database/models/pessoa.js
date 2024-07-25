3560db12edd435c662c885395ddb3a6a0ab43ec3
Victor Ozoterio <victorozoterio@gmail.com>
2024-07-25 11:23:16 -0300

    feat: criando um escopo padrão

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -26,6 +26,7 @@ module.exports = (sequelize, DataTypes) => {
 			modelName: "Pessoa",
 			tableName: "pessoas",
 			paranoid: true,
+			defaultScope: { where: { ativo: true } },
 		},
 	);
 	return Pessoa;
