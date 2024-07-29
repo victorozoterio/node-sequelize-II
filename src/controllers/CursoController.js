e721bc46e8e37756d6d35a55cfca166fc772a699
Victor Ozoterio <victorozoterio@gmail.com>
2024-07-29 17:44:50 -0300

    feat: utilizando query params

--- src/controllers/CursoController.js
+++ src/controllers/CursoController.js
@@ -20,6 +20,13 @@ class CursoController extends Controller {
     data_inicial ? (where.data_inicio[Op.gte] = data_inicial) : null;
     // se existir data final, idem
     data_final ? (where.data_inicio[Op.lte] = data_final) : null;
+
+    try {
+      const listaCursos = await cursoServices.pegaTodosOsRegistros(where);
+      return res.status(200).json(listaCursos);
+    } catch (erro) {
+      return res.status(500).json({ erro: erro.message });
+    }
   }
 }
 
