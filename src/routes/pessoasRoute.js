2f3c9f93c77bfc2f9d730ebe49786158bafa86ed
Victor Ozoterio <victorozoterio@gmail.com>
2024-07-31 16:23:21 -0300

    feat: agrupando registros com sequelize.literal

--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -22,6 +22,9 @@ router.get('/pessoas/:estudante_id/matriculas/todos', (req, res) =>
 router.get('/pessoas/:estudante_id/matriculas/confirmadas', (req, res) =>
   matriculaController.pegaMatriculasPorEstudante(req, res),
 );
+router.get('/pessoas/matriculas/lotadas', (req, res) =>
+  matriculaController.pegaCursosLotados(req, res),
+);
 router.get('/pessoas/:estudante_id/matriculas/:id', (req, res) =>
   matriculaController.pegaUm(req, res),
 );
