91a0943d9237ef122aa64cd0e2c1651908bed742
Victor Ozoterio <victorozoterio@gmail.com>
2024-07-31 20:24:15 -0300

    feat: implementando transações

--- src/services/Services.js
+++ src/services/Services.js
@@ -29,9 +29,10 @@ class Services {
     return dataSource[this.model].create(dadosDoRegistro);
   }
 
-  async atualizaRegistro(dadosAtualizados, where) {
+  async atualizaRegistro(dadosAtualizados, where, transacao = {}) {
     const listadeRegistrosAtualizados = await dataSource[this.model].update(dadosAtualizados, {
       where: { ...where },
+      transaction: transacao,
     });
     if (listadeRegistrosAtualizados[0] === 0) {
       return false;
